81d00ff6296fdf16551cc37ee9cfe36a0e72d678
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-04 17:33:48 +0300

    Создала контроллеры и роуты для авторизации и создания пользователя, сделала роуты

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const user = require('./ routes/users');
 const movies = require('./ routes/movies');
+const { login, createUser } = require('./controllers/user');
 const auth = require('./middlewares/auth');
 
 const app = express();
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use('/users', auth, user);
 app.use('/movies', auth, movies);
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,21 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const { NODE_ENV, JWT_SECRET } = process.env;
+      const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key';
+      const token = jwt.sign({ _id: user._id }, secretKey, { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch(() => {
+      next(new InvalidCredentialsError('Неверный логин или пароль'));
+    });
+};
 
 module.exports.getUser = (req, res, next) => {
   let { userId } = req.params;
@@ -18,6 +35,36 @@ module.exports.getUser = (req, res, next) => {
         next(new IncorrectDataError('Переданы некорректные данные при возврате пользователя по _id'));
         return;
       }
+      next(err);
+    });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hashedPassword) => User.create({
+      name,
+      email,
+      password: hashedPassword,
+    }))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new IncorrectDataError('Переданы некорректные данные при создании пользователя'));
+        return;
+      }
+
+      if (err.name === 'MongoError' && err.code === 11000) {
+        next(new DuplicateDataError('Такой email уже существует'));
+        return;
+      }
+
       next(err);
     });
 };

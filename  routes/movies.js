f4d9966db80cc6b9abe59b1e3743be16d71826e5
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-07 20:49:07 +0300

    Установила celebrate, валиди ровала данные

---  routes/movies.js
+++  routes/movies.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getSavedMovies, createMovie, deleteMovie } = require('../controllers/movie');
 
 router.get('/', getSavedMovies);
 
-router.post('/', createMovie);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().uri(),
+    trailer: Joi.string().required().uri(),
+    thumbnail: Joi.string().required().uri(),
+    movieId: Joi.string().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), createMovie);
 
-router.delete('/:movieId', deleteMovie);
+router.delete('/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required(),
+  }),
+}), deleteMovie);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi, errors } = require('celebrate');
 const user = require('./ routes/users');
 const movies = require('./ routes/movies');
 const { login, createUser } = require('./controllers/user');
@@ -22,11 +23,25 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 const app = express();
 app.use(express.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use('/users', auth, user);
 app.use('/movies', auth, movies);
+
+app.use(errors());
 
 app.use((req, res, next) => {
   next(new NotFoundError('Ресурс запроса не найден'));
